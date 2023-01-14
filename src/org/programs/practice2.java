add0aaca130cc2ec2004796d1930c7a4213fc7e3
sriramjhansi <jhansisriram22@gmail.com>
2023-01-14 08:43:48 +0530

    newly updated code

--- src/org/programs/practice2.java
+++ src/org/programs/practice2.java
@@ -11,6 +11,9 @@ public class practice2 {
 		System.out.println("test4");
 
 		System.out.println("test5");
+		System.out.println("test6");
+		System.out.println("test7");
+		System.out.println("test8");
 
 	}
 
